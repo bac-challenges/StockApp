9fc39cc1c46df70f898c88110088904849affe50
emile <emile@blueantcorp.com>
2026-04-07 11:33:01 +0300

    init
    Signed-off-by: emile <emile@blueantcorp.com>

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by emile on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
