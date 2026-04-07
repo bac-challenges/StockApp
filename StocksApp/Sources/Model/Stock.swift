02d2a01ff67a829a4f7a0db2c1bab902f1c518f0
emile <emile@blueantcorp.com>
2026-04-07 12:05:44 +0300

    add stock model
    Signed-off-by: emile <emile@blueantcorp.com>

---
+++ StocksApp/Sources/Model/Stock.swift
@@ -0,0 +1,17 @@
+//
+//  Stock.swift
+//  StocksApp
+//
+//  Created by emile on 07/04/2026.
+//
+
+import Foundation
+
+struct Stock {
+
+    let symbol: String
+    let description: String
+    
+    let price: Double
+    let previousPrice: Double
+}
