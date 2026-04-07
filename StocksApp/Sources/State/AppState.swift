d085a163ad3074643177353c8b083d49d3fb868d
emile <emile@blueantcorp.com>
2026-04-07 18:11:09 +0300

    start/stop methods
    Signed-off-by: emile <emile@blueantcorp.com>

--- StocksApp/Sources/State/AppState.swift
+++ StocksApp/Sources/State/AppState.swift
@@ -22,6 +22,7 @@ protocol AppStateProtocol: Observable {
 final class AppState: AppStateProtocol {
     
     private(set) var connectionState: ConnectionState = .disconnected
+    private(set) var lifecycle: Lifecycle = .stopped
     let isRunning = false
     
     let stockStore: StockStore
@@ -30,7 +31,22 @@ final class AppState: AppStateProtocol {
     init() {
         self.stockStore = StockStore(stocks: Stock.stocks)
     }
+}
 
-    func start() {}
-    func stop() {}
+// MARK: - Lifecycle[
+extension AppState {
+
+    enum Lifecycle {
+        case stopped
+        case running
+    }
+    
+    func start() {
+        guard lifecycle == .stopped else { return }
+        lifecycle = .running
+    }
+    
+    func stop() {
+        lifecycle = .stopped
+    }
 }
